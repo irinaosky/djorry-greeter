85df93f7b9d7f33e231c54d6f5e6223d01eb7e92
Irina Ovruchevskaya <vondonsirius@gmail.com>
2026-09-13 21:24:07 +0200

    Add datetime for Djorry

--- djorry_greeter.py
+++ djorry_greeter.py
@@ -1,4 +1,9 @@
 import random
+import datetime
+
+current_time = datetime.datetime.now()
+hour = current_time.hour
+
 
 moods = [
     "счастливый", 
@@ -31,3 +36,16 @@ greeting = random.choice(greetings)
 
 print(greeting.format(name=name))
 print(f"Сегодня Djorry: {mood}!")
+
+if hour == 7:
+    print("Джорри, пора на утреннюю прогулку!")
+elif hour == 14:
+    print("Джорри, пора обедать!")
+elif hour == 15:
+    print("Джорри, получи вкусняшку!")
+elif hour == 19:
+    print("Джорри, пора на вечернюю прогулку!")
+elif hour == 21:
+    print("Джорри, пора ужинать!")
+else:
+    print("Сейчас у меня свободное время!")
