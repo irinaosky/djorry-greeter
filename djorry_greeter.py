8c906f24eab181fc16e6d5ba0a682a05163a2a67
Irina Ovruchevskaya <vondonsirius@gmail.com>
2026-09-15 23:17:08 +0200

    Add Djorry's nighttime schedule

--- djorry_greeter.py
+++ djorry_greeter.py
@@ -4,7 +4,6 @@ import datetime
 current_time = datetime.datetime.now()
 hour = current_time.hour
 
-
 moods = [
     "счастливый", 
     "грустный", 
@@ -23,30 +22,33 @@ greetings = [
     "Гав! {name}, Джорри хочет обнять тебя всеми четырьмя лапами!",
 ]
 
-name = input("Как тебя зовут?")
-
-special_names = {
-    "Irina": "Мама",
-    "Sergey": "Папа",
-    "Djorry": "Главный Босс",
-    "Victor": "Мой лучший друг"
-}
-name = special_names.get(name, name)
-
-greeting = random.choice(greetings)
-
-print(greeting.format(name=name))
-print(f"Сегодня Djorry: {mood}!")
-
-if hour == 7:
-    print("Джорри, пора на утреннюю прогулку!")
-elif hour == 14:
-    print("Джорри, пора обедать!")
-elif hour == 15:
-    print("Джорри, получи вкусняшку!")
-elif hour == 19:
-    print("Джорри, пора на вечернюю прогулку!")
-elif hour == 21:
-    print("Джорри, пора ужинать!")
+if hour >= 22 or hour < 7:
+    print("Джорри спит, пожалуйста не беспокоить!")
 else:
-    print("Сейчас у меня свободное время!")
+    name = input("Как тебя зовут?")
+
+    special_names = {
+        "Irina": "Мама",
+        "Sergey": "Папа",
+        "Djorry": "Главный Босс",
+        "Victor": "Мой лучший друг"
+    }
+    name = special_names.get(name, name)
+
+    greeting = random.choice(greetings)
+
+    print(greeting.format(name=name))
+    print(f"Сегодня Djorry: {mood}!")
+
+    if hour == 7:
+        print("Джорри, пора на утреннюю прогулку!")
+    elif hour == 14:
+        print("Джорри, пора обедать!")
+    elif hour == 15:
+        print("Джорри, получи вкусняшку!")
+    elif hour == 19:
+        print("Джорри, пора на вечернюю прогулку!")
+    elif hour == 21:
+        print("Джорри, пора ужинать!")
+    else:
+        print("Сейчас у меня свободное время!")
